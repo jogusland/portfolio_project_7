100e5db87c87232f48f769f1b8b211ce0b5d209e
Jogu <jogupersonnel@gmail.com>
2021-04-13 23:09:02 -0700

    html and css needs work

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const alertBanner = document.getElementById("alert");
 //create the html for the banner
 alertBanner.innerHTML = `
     <div class="alert-banner">
-        <p><strong>Alert:</strong> You have <strong>6</strong> overdue taskes to complete</p>
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
         <p class="alert-banner-close">x</p>
     </div>
     `
@@ -14,3 +14,14 @@ alertBanner.addEventListener('click', e => {
         alert.style.display = "none";
     }
 });
+
+const trafficCanvas = document.getElementById('traffic-chart');
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: 'rgba(116, 119, 191, .3)',
+      borderWidth: 1,
+    }]
+};

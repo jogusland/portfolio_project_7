0a226d4f5cb94bdef5b867761e674ffa532090c8
Jogu <jogupersonnel@gmail.com>
2021-04-17 13:23:59 -0700

    working on js

--- app.js
+++ app.js
@@ -18,10 +18,33 @@ alertBanner.addEventListener('click', e => {
 const trafficCanvas = document.getElementById('traffic-chart');
 
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    labels: ["4-10", "10-25", "20-5", "5-13", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
-      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-      backgroundColor: 'rgba(116, 119, 191, .3)',
+      data: [751, 1251, 1200, 2200, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: 'rgba(126, 119, 191, .3)',
       borderWidth: 1,
     }]
 };
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+      duration: 0
+    },
+    scales: {
+      y: [{
+        ticks: {
+          beginAtZero: true
+    } }]
+    }, 
+    plugins: {
+    legend: {
+        display: false
+      }
+ } };
+
+ let trafficChart = new CharacterData(trafficCanvas, {
+     type: 'line',
+     data: trafficData,
+     options: trafficOptions
+ });
